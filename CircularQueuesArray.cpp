cfd62cc5d522d88943b3c5d5da68b22c3306808f
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-30 11:41:10 +0700

    catch

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -129,7 +129,9 @@ int main()
             }
             }            
         }
-        
+        catch (exception& e) {
+            cout << "Check for the values entered." << endl;
+        }
     }
     return 0;
 }
