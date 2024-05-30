031dc5c2511bdde2ae3d337a8ca098355e67e3df
AfinAlfian <alfinalfiansyah528@gmail.com>
2024-05-30 11:03:51 +0700

    mengecek jika front di posisi terakhir

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -47,6 +47,12 @@ public:
             FRONT = -1;
             REAR = -1;
         }
+        else {
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
     }
 };
 
